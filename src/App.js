834b2f2ffba777bacefd71929a83e153fedc3de6
LapatradaCh <mewlapatrada@gmail.com>
2025-10-10 13:31:23 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
+// App.js
 import React from "react";
-import { Routes, Route } from 'react-router-dom';
-import Login from './components/1';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./components/1";
 
 function App() {
+  return (
+    <Router>
       <Routes>
-      <Route path="/" element={<Login />} />
-    </Routes>
+        <Route path="./components/1" element={<Login />} />
+        <Route path="*" element={<Login />} /> {/* Default หน้า Login */}
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
-
