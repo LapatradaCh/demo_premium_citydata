b89bca1fa41d0a5fa8c92af1e252a6fba9da2c7b
LapatradaCh <mewlapatrada@gmail.com>
2025-10-14 09:47:45 +0700

    App.js

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./components/1";
+import Dashboard from "./components/Dashboard";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="./components/1" element={<Login />} />
-        <Route path="*" element={<Login />} /> {/* Default หน้า Login */}
+        <Route path="/" element={<Login />} /> {/* หน้า Login */}
+        <Route path="/dashboard" element={<Dashboard />} /> {/* หน้า Dashboard */}
       </Routes>
     </Router>
   );
