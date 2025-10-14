1b5d2687a2807cc975de4a7af1eac754260047ba
LapatradaCh <mewlapatrada@gmail.com>
2025-10-14 09:23:32 +0700

    fix error icon

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
+// App.js
 import React from "react";
-import Login from './components/1';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./components/1";
 
 function App() {
-  return <Login />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="./components/1" element={<Login />} />
+        <Route path="*" element={<Login />} /> {/* Default หน้า Login */}
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
