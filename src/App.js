77753497bbe6866c6d5f6769d301809505817dba
LapatradaCh <mewlapatrada@gmail.com>
2025-10-14 09:10:26 +0700

    App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-// App.js
-import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Login from "./components/1";
+import Login from "./Login";
+import Dashboard from "./Dashboard";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="./components/1" element={<Login />} />
-        <Route path="*" element={<Login />} /> {/* Default หน้า Login */}
+        <Route path="/" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </Router>
   );
