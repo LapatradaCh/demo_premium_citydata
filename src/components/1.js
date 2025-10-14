7f08c1d7679b0ac0eea3bced6789d8865c55a2d8
Taned_Wonpoo <151303397+TanedW@users.noreply.github.com>
2025-10-14 16:42:02 +0700

    Update API URL in 1.js

--- src/components/1.js
+++ src/components/1.js
@@ -6,7 +6,7 @@ import { auth, googleProvider, facebookProvider } from "./firebaseConfig";
 import { signInWithPopup, FacebookAuthProvider } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
 
-const DB_API = "https://780fc4c943c0.ngrok-free.app/users"; // URL API ของคุณ
+const DB_API = "https://myapi-331445071173.asia-southeast1.run.app/users"; // URL API ของคุณ
 
 const Login = () => {
   const navigate = useNavigate();
