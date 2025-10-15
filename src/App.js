ea5de297e0e246a5f0dd37d6b90312ff965c3c94
LapatradaCh <mewlapatrada@gmail.com>
2025-10-15 08:50:17 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
-import Dashboard from "./components/Home";
+import  Home from "./components/Home";
 function App() {
   return (
     <Router>
