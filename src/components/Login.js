bb1af4fca0cdd522b079b1c8304c583165e5da82
Taned_Wonpoo <151303397+TanedW@users.noreply.github.com>
2025-10-15 09:14:37 +0700

    Remove DisplayName from userData on LINE login
    Removed DisplayName from userData object during LINE login.

--- src/components/Login.js
+++ src/components/Login.js
@@ -33,9 +33,10 @@ const Login = () => {
         const profile = await liff.getProfile();
         const userData = {
           Email: profile.userId + "@line.me",
+          First_Name: profile.displayName,
+          Last_Name: "-",
           Provider: "line",
           Provider_ID: profile.userId,
-          DisplayName: profile.displayName,
         };
 
         console.log("ล็อกอิน LINE สำเร็จ:", userData);
