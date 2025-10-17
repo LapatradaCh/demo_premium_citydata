6eda0646a55ff888d0a39855522ef86c381b290a
LapatradaCh <mewlapatrada@gmail.com>
2025-10-17 11:47:31 +0700

    Refactor JSX structure in Login component

--- src/components/Login.js
+++ src/components/Login.js
@@ -197,18 +197,17 @@ const Login = () => {
             </button>
             
             {/* ... JSX ส่วนที่เหลือเหมือนเดิม ... */}
-             < >
-              <div className="links">
-                <a href="https://www.traffy.in.th/Traffy-Fondue-247430d4aa7b803b835beb9ee988541f" target="_blank" rel="noopener noreferrer">
-                  คู่มือการใช้งาน
+            <div className="links">
+              <a href="https://www.traffy.in.th/Traffy-Fondue-247430d4aa7b803b835beb9ee988541f" target="_blank" rel="noopener noreferrer">
+                คู่มือการใช้งาน
+              </a>
+              <p className="contact">
+                <a href="line://ti/p/@fonduehelp" target="_blank" rel="noopener noreferrer">
+                  ติดต่อสอบถาม
                 </a>
-                <p className="contact">
-                  <a href="line://ti/p/@fonduehelp" target="_blank" rel="noopener noreferrer">
-                    ติดต่อสอบถาม
-                  </a>
-                </p>
-              </div>
-            </>
+              </p>
+            </div>
+          </>
         )}
       </div>
     </div>
