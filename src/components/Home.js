b6687e154b05c1214b111a8b35bd6b6eff739d7b
Taned_Wonpoo <151303397+TanedW@users.noreply.github.com>
2025-10-21 19:11:44 +0700

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -114,8 +114,8 @@ const ReportTable = () => (
   </div>
 );
 
-// Dashboard
-const Dashboard = () => {
+// Home
+const Home = () => {
   const [activeTab,setActiveTab] = useState("รายการแจ้ง");
   const [dropdownOpen,setDropdownOpen] = useState(null);
   const tabs = ["แผนที่","หน่วยงาน","รายการแจ้ง","สถิติ","ตั้งค่า"];
@@ -272,4 +272,4 @@ const Dashboard = () => {
   );
 };
 
-export default Dashboard;
+export default Home;
