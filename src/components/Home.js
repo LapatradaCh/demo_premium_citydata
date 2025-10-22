ba103d371e9086b0db7b496cb94c58f4792bd5e5
LapatradaCh <mewlapatrada@gmail.com>
2025-10-23 00:24:44 +0700

    refactor carlendar

--- src/components/Home.js
+++ src/components/Home.js
@@ -35,11 +35,9 @@ const DateFilter = () => {
     <div style={{ position: "relative" }}>
       <button className="time-range-button" onClick={() => setShow(!show)}>{formatDate(date)}</button>
       {show && (
-        <calendar-date class="cally bg-base-100 border border-base-300 shadow-lg rounded-box">
-  <svg aria-label="Previous" className="fill-current size-4" slot="previous" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="currentColor" d="M15.75 19.5 8.25 12l7.5-7.5"></path></svg>
-  <svg aria-label="Next" className="fill-current size-4" slot="next" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="currentColor" d="m8.25 4.5 7.5 7.5-7.5 7.5"></path></svg>
-  <calendar-month></calendar-month>
-</calendar-date>
+        <div className="calendar-popup">
+          <Calendar onChange={d => { setDate(d); setShow(false); }} value={date || new Date()} />
+        </div>
       )}
     </div>
   );
