bd00fad539319bf34f76b025581058674663cb90
Taned_Wonpoo <151303397+TanedW@users.noreply.github.com>
2025-10-22 08:26:17 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,28 +1,37 @@
 // App.js
+import React from 'react';
+import { Routes, Route } from 'react-router-dom'; // <-- 1. Import Routes และ Route
 import Home from './components/Home';
 import Home1 from './components/Home1';
 import Login from './components/Login';
-import ProtectedRoute from './components/ProtectedRoute'; // <-- 1. Import ยามเข้ามา
+import ProtectedRoute from './components/ProtectedRoute'; // <-- Import ยามเข้ามา
 
-<Routes>
-  <Route path="/" element={<Login />} />
-  
-  {/* หน้าที่ต้อง Login ถึงจะเห็น */}
-  <Route 
-    path="/home1" 
-    element={
-      <ProtectedRoute>
-        <Home1 />
-      </ProtectedRoute>
-    } 
-  />
-  <Route 
-    path="/home" 
-    element={
-      <ProtectedRoute>
-        <Home />
-      </ProtectedRoute>
-    } 
-  />
-</Routes>
+// 2. สร้าง Function App()
+function App() {
+  // 3. ใช้ return เพื่อคืนค่า JSX
+  return (
+    <Routes>
+      <Route path="/" element={<Login />} />
+      
+      {/* หน้าที่ต้อง Login ถึงจะเห็น */}
+      <Route 
+        path="/home1" 
+        element={
+          <ProtectedRoute>
+            <Home1 />
+          </ProtectedRoute>
+        } 
+      />
+      <Route 
+        path="/home" 
+        element={
+          <ProtectedRoute>
+            <Home />
+          </ProtectedRoute>
+        } 
+      />
+    </Routes>
+  );
+}
+
 export default App;
