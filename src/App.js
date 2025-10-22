ddb71289d5a09c22500d34474f4e9cf6081a2888
Taned_Wonpoo <151303397+TanedW@users.noreply.github.com>
2025-10-22 08:20:45 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 // App.js
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Login from "./components/Login";
-import  Home from "./components/Home";
-import Home1 from "./components/Home1"
-function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Login />} /> {/* หน้า Login */}
-        <Route path="/Home" element={<Home />} /> {/* หน้า Home */}
-        <Route path="/Home1" element={<Home1 />} /> {/* หน้า Home */}
-      </Routes>
-    </Router>
-  );
-}
-export default App;
+import Dashboard from './components/Dashboard';
+import Home1 from './components/Home1';
+import Login from './components/Login';
+import ProtectedRoute from './components/ProtectedRoute'; // <-- 1. Import ยามเข้ามา
+
+<Routes>
+  <Route path="/" element={<Login />} />
+  
+  {/* หน้าที่ต้อง Login ถึงจะเห็น */}
+  <Route 
+    path="/home1" 
+    element={
+      <ProtectedRoute>
+        <Home1 />
+      </ProtectedRoute>
+    } 
+  />
+  <Route 
+    path="/home" 
+    element={
+      <ProtectedRoute>
+        <Home />
+      </ProtectedRoute>
+    } 
+  />
+</Routes>
