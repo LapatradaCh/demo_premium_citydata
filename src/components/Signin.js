3a57efd9a44fcfc02217840a17b59471e9c78b01
Taned_Wonpoo <151303397+TanedW@users.noreply.github.com>
2025-10-24 15:12:35 +0700

    Rename signin.css to Signin.css

--- src/components/Signin.js
+++ src/components/Signin.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import "./css/signin.css";
+import "./css/Signin.css";
 import logo from "./traffy.png";
 import { FaSignOutAlt as LogOut } from "react-icons/fa";
 
