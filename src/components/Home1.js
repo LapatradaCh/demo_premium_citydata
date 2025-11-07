9a36ea933d3763180cc75bca2133216c115f5e84
Taned_Wonpoo <151303397+TanedW@users.noreply.github.com>
2025-11-07 16:23:07 +0700

    Update Home1.js

--- src/components/Home1.js
+++ src/components/Home1.js
@@ -46,6 +46,7 @@ const Home1 = () => {
           img: item.url_logo, 
           badge: null 
         }));
+         console.log('Raw data from API:', formattedData);
 
         setAllAgencies(formattedData);
         setFilteredAgencies(formattedData);
