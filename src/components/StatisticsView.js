73e7c51729e0454aedc994cb36c3ab6a0ea796c1
LapatradaCh <mewlapatrada@gmail.com>
2025-12-02 15:11:59 +0700

    Update StatisticsView.js

--- src/components/StatisticsView.js
+++ src/components/StatisticsView.js
@@ -19,7 +19,7 @@ import {
   ComposedChart
 } from 'recharts';
 
-import styles from './StatisticsView.module.css';
+import styles from './css/StatisticsView.module.css';
 
 // --- Configuration ---
 const STATUS_COLORS = {
