df9043858d984403982d93c9c444f8a55ede4599
Taned_Wonpoo <151303397+TanedW@users.noreply.github.com>
2025-12-03 09:27:30 +0700

    Update ReportDetail.js

--- src/components/ReportDetail.js
+++ src/components/ReportDetail.js
@@ -21,7 +21,8 @@ const IconArrowRight = () => (<svg width="18" height="18" fill="none" stroke="cu
 const IconUsers = () => (<svg width="18" height="18" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M18 9v3m0 0v3m0-3h3m-3 0h-3m-2-5a4 4 0 11-8 0 4 4 0 018 0zM3 20a6 6 0 0112 0v1H3v-1z"></path></svg>);
 const IconX = () => (<svg width="18" height="18" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2.5" d="M6 18L18 6M6 6l12 12"></path></svg>);
 
-const ReportDetail = ({ reportId, onGoToInternalMap }) => {
+const ReportDetail = ({onGoToInternalMap }) => {
+  const reportId = localStorage.getItem("selectedCaseId");
   
   // Data States
   const [caseInfo, setCaseInfo] = useState(null);
