c52144c12b4ce34bece7df583f72b4567729f1c2
LapatradaCh <mewlapatrada@gmail.com>
2026-03-09 15:19:37 +0700

    Remove orgImage state from LogoSetupForm
    Remove unused state for orgImage.

--- src/components/CreateOrg.js
+++ src/components/CreateOrg.js
@@ -80,7 +80,6 @@ const QuickCreatePage = ({
  * =================================================================
  */
 const LogoSetupForm = ({ onSave, orgId }) => {
-  const [orgImage, setOrgImage] = useState(null);
   const [orgImagePreview, setOrgImagePreview] = useState(null);
   const [isSaving, setIsSaving] = useState(false);
 
