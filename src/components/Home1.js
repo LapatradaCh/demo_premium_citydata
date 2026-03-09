b3be59fcabdafb0c190023f95d2c71a10b82500f
LapatradaCh <mewlapatrada@gmail.com>
2026-03-09 15:41:46 +0700

    Fix import statement for lucide-react icons

--- src/components/Home1.js
+++ src/components/Home1.js
@@ -3,7 +3,7 @@ import { useNavigate } from 'react-router-dom';
 import styles from './css/Home1.module.css'; // ตรวจสอบ path CSS
 
 // Import Icons for Content (Search & Cards)
-import { Search, X } from 'lucide-react';ฃ
+import { Search, X } from 'lucide-react';
 // Import Icons for Bottom Menu & Logout
 import {
   FaMapMarkedAlt,
